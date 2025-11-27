ff3bcd3e739e3b20e03984af76c5c98d148de143
Katie Karcheski <katie.karcheski@hotmail.co.uk>
2025-11-27 21:33:31 +0000

    Refactor HTML and CSS structure; remove unused pages and update modal functionality

--- Backend/public/scripts/myplants.js
+++ Backend/public/scripts/myplants.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('plant-form');
     const cardsContainer = document.getElementById('card_grid');
     const addPlantCard = document.getElementById('add_card');
-    const modalOverlay = document.getElementById('form-modal-overlay');
-    const closeModalBtn = document.getElementById('close-modal-btn');
+    const modalOverlay = document.getElementById('form_modal_overlay');
+    const closeModalBtn = document.getElementById('close_modal_btn');
 
 // Function to open the modal
 addPlantCard.addEventListener('click', () => {
